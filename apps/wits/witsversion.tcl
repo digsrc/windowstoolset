9d63560b9dcef46562e0b9de8a0dc894dd29fdc7
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2011-07-23 17:30:08 +0530

    More tinkering with install bitmaps

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.0.15" 
+variable version "3.0.16" 
 variable release_type "b" 
 } 
