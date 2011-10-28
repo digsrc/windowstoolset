31d622d1159e7675f06842be1f2f855a51dc83e8
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2011-10-28 19:16:50 +0530

    Bumped version to 3.0.24

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.0.22" 
-variable release_type "b" 
+variable version "3.0.24" 
+variable release_type "" 
 } 
