7b484ab2c6e3b82bb611f1094e43317c4db2cf6a
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-03-08 15:12:58 +0530

    3.1b5

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.1.4" 
+variable version "3.1.5" 
 variable release_type "b" 
 } 
