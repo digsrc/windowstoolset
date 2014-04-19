0304830b272215f39d04627b3a04a12934f52547
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-04-19 18:14:54 +0530

    witscpu.tcl missing from makefile, not copied into distro Fixed missing icons from distro Bump version to 3.1.10

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.1.8" 
+variable version "3.1.9" 
 variable release_type "" 
 } 
