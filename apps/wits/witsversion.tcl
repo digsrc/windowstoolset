06aed7c38fcb345fe4ba15a57406dc6eac068e29
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-05-20 14:17:39 +0530

    Update tclkits to twapi 4.0.61. Wits version 3.1.15

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.1.14" 
+variable version "3.1.15" 
 variable release_type "" 
 } 
