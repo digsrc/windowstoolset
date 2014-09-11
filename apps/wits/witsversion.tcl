4eeab45b71661cc1ec45ef5742bd804037ca5228
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-09-11 20:12:50 +0530

    Bump to 3.2.2

--- apps/wits/witsversion.tcl
+++ apps/wits/witsversion.tcl
@@ -2,6 +2,6 @@
 namespace eval ::wits::app { 
 variable name "WiTS" 
 variable long_name "Windows Inspection Tool Set" 
-variable version "3.1.17" 
+variable version "3.2.2" 
 variable release_type "" 
 } 
